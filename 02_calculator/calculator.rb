3dcebdc0d0e7ef5015f1e8e54145ef07936e3a92
bleepblopblorpblopbloop <kdchorley@hotmail.com>
2019-07-08 22:19:10 +0200

    02_calc complete

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add(x, y)
+    (x + y)
+end
+
+def subtract(x, y)
+    (x - y)
+end
+
+
+def sum(an_array)
+      an_array.inject(0){|sum,x| sum + x }
+end
+
+def multiply(an_array)
+    an_array.inject(0){|multiply,x| multiply * x }
+end
