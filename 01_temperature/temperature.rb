7b8fdc506b931148ba94b76e4cb72bcc52156c50
bleepblopblorpblopbloop <kdchorley@hotmail.com>
2019-07-08 21:53:02 +0200

    Project Ruby: 00 & 01

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,15 @@
-#write your code here
+def ftoc(x)
+    (x - 32)*(5/9)
+end
+
+def ftoc(x)
+    ((x - 32)*5)/9
+end
+
+def ctof(x)
+    (x * 9/5) + (32)
+end
+
+def ctof(x)
+    (x * 9.0/5.0) + (32.0)
+end
