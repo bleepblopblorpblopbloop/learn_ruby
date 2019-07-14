b0cf59cab617e7e0fd962da75cc6496a0f676395
bleepblopblorpblopbloop <kdchorley@hotmail.com>
2019-07-14 12:51:36 +0200

    pig_latin complete

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+$vowels = "aeiou"
+
+def consonant(s)
+  n = 0
+     while n < s.length
+        if $vowels.include?(s[n]) && s[n-1..n] != "qu"
+           return "#{s[n..-1]}#{s[0..n-1]}ay"
+        else
+           n += 1
+        end
+     end
+  end
+def translate(s)
+    s.split.map{ |s| $vowels.include?(s[0]) ? "#{s}ay" : consonant(s) }.join(" ")
+end
