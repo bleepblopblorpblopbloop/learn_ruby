bc4142609edafd72f1a69a5d7e65270bf72b37f9
bleepblopblorpblopbloop <kdchorley@hotmail.com>
2019-07-16 07:48:01 +0200

    book completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,11 @@
 class Book
-# write your code here
-end
+
+    attr_accessor :title
+  
+    def title
+      little_words = %w[a an and the of in]
+      @title.split.each_with_index.map { |word, index| little_words.include?(word) && index != 0 ? word : word.capitalize}.join(" ")
+    end
+  
+  end
+  
